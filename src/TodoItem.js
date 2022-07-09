e2838eaf165d6195bea2403352f35b936ea03caf
thevkscode <singhvivek02099@gmail.com>
2022-07-10 02:30:02 +0530

    Event Handler added to check and uncheck box

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,7 +6,14 @@ export default class TodoItem extends React.Component{
            id: 1,
             text: "My Task",
            completed: true
-        };
+        }
+    }
+    check=()=>{
+        this.setState((prevState)=>{
+            return{
+                completed:!(prevState.completed)
+            }
+        });
     }
      // eslint-disable-next-line
 render(){
@@ -15,7 +22,7 @@ render(){
         <div className='todo-list'>
             <h1 style={{fontSize:50}}>My Daily Goals</h1>
             <div>
-            <input type='CheckBox' id={id} name={text} value="task" checked={completed}/>
+            <input className='task-checkbox' onClick={this.check} type='CheckBox' id={id} name={text} value="task" checked={completed} />
             <label style={{fontSize:20}}>{text}</label>
             </div>
 
